21653dba1eac34aefbad24337a74bae35cadc4fc
likhilliki <likhilgowda89@gmail.com>
2025-06-06 12:23:39 +0530

    Create index.ts

---
+++ api/index.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+import serverless from 'serverless-http';
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Express API working on Vercel!' });
+});
+
+module.exports = app;
+module.exports.handler = serverless(app);
